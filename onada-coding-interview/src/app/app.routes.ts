ded6fb498dcab60d262df8bd15cb1576181cff85
dayoonasanya <aonasanya@mail.dccc.edu>
2025-06-21 19:43:23 -0400

    Enable dynamic routing for challenges by track and id

--- onada-coding-interview/src/app/app.routes.ts
+++ onada-coding-interview/src/app/app.routes.ts
@@ -1,32 +1,40 @@
-import { adminGuard } from './admin-login/admin.guard';
 import { Routes } from '@angular/router';
 import { InterviewLandingComponent } from './interview-landing/interview-landing.component';
+import { adminGuard } from './admin-login/admin.guard';
 
 export const routes: Routes = [
   {
     path: '',
-    component: InterviewLandingComponent
+    component: InterviewLandingComponent,
   },
   {
     path: 'candidate-form',
     loadComponent: () =>
-      import('./candidate-form/candidate-form.component').then(m => m.CandidateFormComponent)
+      import('./candidate-form/candidate-form.component').then(
+        (m) => m.CandidateFormComponent
+      ),
   },
   {
-    path: 'challenge',
+    path: 'challenge/:track/:id',
     loadComponent: () =>
-      import('./challenge/challenge.component').then(m => m.ChallengeComponent)
+      import('./challenge/challenge.component').then(
+        (m) => m.ChallengeComponent
+      ),
   },
   {
     path: 'admin-login',
     loadComponent: () =>
-      import('./admin-login/admin-login.component').then(m => m.AdminLoginComponent)
+      import('./admin-login/admin-login.component').then(
+        (m) => m.AdminLoginComponent
+      ),
   },
   {
     path: 'admin',
     canActivate: [adminGuard],
     loadComponent: () =>
-      import('./admin-dashboard/admin-dashboard.component').then(m => m.AdminDashboardComponent)
-  }
+      import('./admin-dashboard/admin-dashboard.component').then(
+        (m) => m.AdminDashboardComponent
+      ),
+  },
 ];
 
